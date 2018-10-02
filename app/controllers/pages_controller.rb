48fbe16265f1a491f9b6291f085ee2194d8e86c5
Meenakshi Radhakrishnan <junnamma@yahoo.com>
2018-10-02 10:42:42 -0400

    Removed unused variable message

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,11 @@
 class PagesController < ApplicationController
   def home
-    @message = "This is the main page"
   end
 
   def search
-    @message = "Search Page"
     @dark_theme = true
   end
 
   def results
-    @message = "Show the results of the search here"
   end
 end
